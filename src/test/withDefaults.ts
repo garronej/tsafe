cf784a7b0a8576f5ea9156ba13d286f78dc26c68
William Thiery <williamalexthiery@hotmail.fr>
2021-04-11 19:38:58 +0200

    shitty commit

--- src/test/withDefaults.ts
+++ src/test/withDefaults.ts
@@ -5,25 +5,22 @@
 import { doExtends } from "../typeSafety/doExtends";
 import { withDefaults } from "../typeSafety/withDefaults";
 
-
 // eslint-disable-next-line @typescript-eslint/no-empty-function
-function notAny(_arg: { __notAnyBrand: unknown; }): void{ }
+function notAny(_arg: { __notAnyBrand: unknown }): void {}
 
+//function test1<T, U, V>() {
 function test1<T, U, V>() {
+    const f: (params: { foo: T; bar: U }) => V = null as any;
 
-    const f: (params: { foo: T; bar: U; }) => V = null as any;
+    const got = withDefaults(f, { "bar": (null as any) as U });
 
-    const got = withDefaults(f, { "bar": null as any as U });
-
-    const expected: (params: { foo: T; bar?: U; }) => V = null as any;
+    const expected: (params: { foo: T; bar: U }) => V = null as any;
 
     //@ts-expect-error
     notAny(got);
     doExtends<typeof got, typeof expected>();
     doExtends<typeof expected, typeof got>();
-
 }
-
 
 function test2<T>() {
     // eslint-disable-next-line @typescript-eslint/ban-types
@@ -31,22 +28,18 @@ function test2<T>() {
 
     //@ts-expect-error
     withDefaults(f, { "foo": null as any });
-
 }
 
-function test3<T, U, V>(){
+function test3<T, U, V>() {
+    const f: (params: { foo?: T; bar: U }) => V = null as any;
 
-    const f: (params: { foo?: T; bar: U; }) => V = null as any;
-
-
-    const got = withDefaults(f, { "bar": null as any as U });
+    const got = withDefaults(f, { "bar": (null as any) as U });
     const expected: (params: { foo?: T; bar?: U }) => V = null as any;
 
     //@ts-expect-error
     notAny(got);
     doExtends<typeof got, typeof expected>();
     doExtends<typeof expected, typeof got>();
-
 }
 
-export { };
+export {};
--- src/typeSafety/withDefaults.ts
+++ src/typeSafety/withDefaults.ts
@@ -1,4 +1,50 @@
+// const { getConstantWp } withDefaults({ getConstant });
 
+export function withDefaults<T extends Record<string, unknown>, U extends keyof T>(
+    f: (args: T) => unknown,
+    injectedArgs: { [Key in U]: T[Key] },
+): typeof f {
+    return null as any;
 
-export function withDefaults(...args: any[]): unknown {
+    /*
+    return (args: { [Key in Exclude<keyof T, keyof U>]: T[Key] }) =>
+        f({ ...args, ...injectedArgs })
+        */
 }
+
+const f = (params: { a: number; b: number }) => {
+    return params;
+};
+
+const { functionWp } = withDefaults(f, { "b": 4 });
+
+const x = functionWp({ "a": 42, "b": 4 });
+
+type A = {
+    a: number;
+    b: number;
+    c: number;
+};
+
+type B = Pick<A, "a" | "b">;
+
+type C = {
+    [Key in Exclude<keyof A, keyof B>]: A[Key];
+};
+
+const c: C = {
+    "c": 123,
+};
+
+const b: B = {
+    "a": 234,
+    "b": 444,
+};
+
+function f2(args: A) {
+    return args;
+}
+
+const f1 = (props: C) => f2({ ...props, ...b });
+
+const f4 = f1({ "c": 44 });
