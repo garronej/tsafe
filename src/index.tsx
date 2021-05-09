56beeb3e63270833c0b8456471d96d834c90ae5e
garronej <joseph.garrone.gj@gmail.com>
2021-05-09 18:09:54 +0200

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,24 @@
 import ReactDOM from "react-dom";
 import { HomepageTemplate } from "homepage-template";
-import type { Data } from "homepage-template";
-
-const data: Data = {
-  "header": {
-    "title": "tsafe",
-    "subTitle": "Leverage the more advanced features of TypeScript",
-    "imageUrl": "https://user-images.githubusercontent.com/6702424/117558675-ab86b780-b07f-11eb-8b7e-e7ed1555a388.png",
-    "topBarProps": {
-      "githubRepoUrl": "https://github.com/garronej/tsafe",
-      "menu": {
-        "items": [],
-        "smallDeviceBreakPointPx": 0
-      }
-    },
-    "buttons": [
-      {
-        "name": "Documentation",
-        "url": "https://docs.tsafe.dev"
-      }
-    ]
-  }
-};
+import heroImgUrl from "assets/img/hero.png";
 
 ReactDOM.render(
-  <HomepageTemplate {...data} />,
-  document.getElementById("root")
+	<HomepageTemplate
+		header={{
+			"title": "tsafe",
+			"subTitle": "Leverage the more advanced features of TypeScript",
+			"imageUrl": heroImgUrl,
+			"topBarProps": {
+				"githubRepoUrl": "https://github.com/garronej/tsafe",
+				"documentationUrl": "https://docs.tsafe.dev",
+			},
+			"buttons": [
+				{
+					"name": "Documentation",
+					"url": "https://docs.tsafe.dev"
+				}
+			]
+		}}
+	/>,
+	document.getElementById("root")
 );
