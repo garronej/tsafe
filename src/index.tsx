b38e3edecf3c39ce34f9770b1f2a2e14dbf36940
garronej <joseph.garrone.gj@gmail.com>
2021-05-09 04:56:45 +0200

    Update

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,20 @@ const data: Data = {
   "header": {
     "title": "tsafe",
     "subTitle": "Leverage the more advanced features of TypeScript",
-    "imageUrl": "https://user-images.githubusercontent.com/6702424/117558675-ab86b780-b07f-11eb-8b7e-e7ed1555a388.png"
+    "imageUrl": "https://user-images.githubusercontent.com/6702424/117558675-ab86b780-b07f-11eb-8b7e-e7ed1555a388.png",
+    "topBarProps": {
+      "githubRepoUrl": "https://github.com/garronej/tsafe",
+      "menu": {
+        "items": [],
+        "smallDeviceBreakPointPx": 0
+      }
+    },
+    "buttons": [
+      {
+        "name": "Documentation",
+        "url": "https://docs.tsafe.dev"
+      }
+    ]
   }
 };
 
