dab0d62eddf420b42a7682cd8001b5d0fac924da
actions <actions@example.com>
2021-05-10 09:11:37 +0000

    Enabling shorter import paths

---
+++ deno_dist/Parameters.ts
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+/** https://docs.tsafe.dev/parameters */
+export type Parameters<T extends ((...args: any[]) => unknown) | null | undefined | false | ""> =
+    T extends (...args: infer P) => any ? P : never;
