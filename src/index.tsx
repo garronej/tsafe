22b66a002d2805378a02e5fe4006ebece378255f
garronej <joseph.garrone.gj@gmail.com>
2021-05-10 04:12:09 +0200

    Add background banner

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import ReactDOM from "react-dom";
 import { HomepageTemplate } from "homepage-template";
 import gifUrl from "assets/img/tsafe.gif";
+import bannerBackgroundUrl from "assets/img/banner_background.jpg";
 
 ReactDOM.render(
 	<HomepageTemplate
 		header={{
+			"background": {
+				"type": "image",
+				"colorOrUrlDark": bannerBackgroundUrl,
+				"colorOrUrlLight": bannerBackgroundUrl
+			},
 			"title": "tsafe",
 			"subTitle": "A collection of utilities that makes your TS code cleaner and safer",
 			"imageUrl": gifUrl,
