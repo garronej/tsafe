7a8941de6ce1838a27aefbcf7b6e7f07b7d69e0d
actions <actions@example.com>
2021-05-13 03:36:58 +0000

    Enabling shorter import paths

---
+++ deno_dist/assert.ts
@@ -0,0 +1,40 @@
+import { overwriteReadonlyProp } from "./lab/overwriteReadonlyProp.ts";
+import { assertIsRefWrapper } from "./zz_internal/assertIsRefWrapper.ts";
+
+/** https://docs.tsafe.dev/assert#error-thrown */
+export class AssertionError extends Error {
+    constructor(msg: string | undefined) {
+        super(`Wrong assertion encountered` + (!msg ? "" : `: "${msg}"`));
+
+        Object.setPrototypeOf(this, new.target.prototype);
+
+        if (!this.stack) {
+            return;
+        }
+
+        try {
+            overwriteReadonlyProp(
+                this,
+                "stack",
+                this.stack
+                    .split("\n")
+                    .filter((...[, i]) => i !== 1 && i !== 2)
+                    .join("\n"),
+            );
+            // eslint-disable-next-line no-empty
+        } catch {}
+    }
+}
+
+/** https://docs.tsafe.dev/assert */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function assert(condition: any, msg?: string): asserts condition {
+    if (assertIsRefWrapper.ref !== undefined) {
+        assertIsRefWrapper.ref = undefined;
+        return;
+    }
+
+    if (!condition) {
+        throw new AssertionError(msg);
+    }
+}
---
+++ deno_dist/withDefaults.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import "./tools/Object.fromEntries.ts";
+
+/** https://docs.tsafe.dev/withdefaults */
+export function withDefaults<Params extends Record<string, unknown>, DefaultKey extends keyof Params, R>(
+    f: (params: Params) => R,
+    defaultParams: keyof Params extends never
+        ? Record<string, never>
+        : { [Key in DefaultKey]: Params[Key] },
+): (
+    params: { [Key in keyof Omit<Params, DefaultKey>]: Params[Key] } & {
+        defaultsOverwrite?: { [Key in DefaultKey]?: [Params[Key]] };
+    },
+) => R {
+    return params => {
+        const { defaultsOverwrite = {}, ...actualParams } = params;
+
+        return f({
+            ...defaultParams,
+            ...actualParams,
+            ...Object.fromEntries(
+                Object.entries(defaultsOverwrite)
+                    .filter(([, value]) => value !== undefined)
+                    .map(([key, value]) => [key, (value as any)[0]]),
+            ),
+        } as any);
+    };
+}
