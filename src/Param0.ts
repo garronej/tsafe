e7a4bdd474fce5f2fcf2bb52b6840c03d450d46f
garronej <joseph.garrone.gj@gmail.com>
2021-05-14 22:58:11 +0200

    Fix build

--- src/Param0.ts
+++ src/Param0.ts
@@ -3,6 +3,5 @@
 import type { Parameters } from "./Parameters";
 
 /** Shorthand for Parameters<typeof f>[0] https://docs.tsafe.dev/param0 */
-export type Param0<
-    T extends ((...args: any[]) => unknown) | null | undefined | false | ""
-> = Parameters<T>[0];
+export type Param0<T extends ((...args: any[]) => unknown) | null | undefined | false | ""> =
+    Parameters<T>[0];
