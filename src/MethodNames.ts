e1b1b781bb927e21d338f69ff518110ecf4bc93f
Joseph Garrone <joseph.garrone.gj@gmail.com>
2021-05-14 23:06:27 +0200

    Update MethodNames.ts

--- src/MethodNames.ts
+++ src/MethodNames.ts
@@ -1,22 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/ban-types */
-/**
- * Takes as type argument an Api interface and return
- * an union type constituted of all the properties names
- * whose values are functions.
- *
- * Example:
- *
- * interface Api {
- *     getFoo(): number;
- *     getBar(): string;
- *     p: object;
- * }
- *
- * MethodNames<Api> is "getFoo" | "getBar"
- *
- */
 
+/** https://docs.tsafe.dev/methodnames */
 export type MethodNames<Api> = NonNullable<
     {
         [Key in keyof Api]: Api[Key] extends ((...args: any[]) => unknown) | undefined ? Key : never;
