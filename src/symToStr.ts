e0e58159799fe78de0e4610e41f7d6e9f4b76797
garronej <joseph.garrone.gj@gmail.com>
2021-06-01 22:56:09 +0200

    Add documentation for symToStr

--- src/symToStr.ts
+++ src/symToStr.ts
@@ -1,4 +1,4 @@
-/** symbolAsString({ foo }) === "foo" */
+/** @see <https://docs.tsafe.dev/main/symtostr> */
 export function symToStr<T extends Record<string, unknown>>(wrap: T): keyof T {
     return Object.keys(wrap)[0];
 }
