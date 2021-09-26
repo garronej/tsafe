668bd65144b8838d4d56668df3be3d18b97276ad
garronej <joseph.garrone.gj@gmail.com>
2021-09-26 17:51:10 +0200

    Fmt

--- src/Equals.ts
+++ src/Equals.ts
@@ -1,23 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-/**
- * https://docs.tsafe.dev/main/equals
- * 
- * Check whether `A1` is equal to `A2` or not.
- * @param A1
- * @param A2
- * @returns [[Boolean]]
- * @example
-
- *
- * To use like:
- *
- * import type { Equals } from "tsafe";
- * import { doExtends } from "tsafe/doExtends";
- *
- * doExtends<Equals<Got,Expected>, 1>();
- *
- */
+/** https://docs.tsafe.dev/main/equals */
 export type Equals<A1 extends any, A2 extends any> = (<A>() => A extends A2 ? 1 : 0) extends <
     A,
 >() => A extends A1 ? 1 : 0
