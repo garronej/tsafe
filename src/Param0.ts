6a637c3255667b7f8e68f708a3694049564edecb
garronej <joseph.garrone.gj@gmail.com>
2021-09-30 09:32:04 +0200

    Param0: Optional params gives their types

--- src/Param0.ts
+++ src/Param0.ts
@@ -2,6 +2,8 @@
 
 import type { Parameters } from "./Parameters";
 
+type NonUndefined<T> = T extends undefined ? never : T;
+
 /** Shorthand for Parameters<typeof f>[0] https://docs.tsafe.dev/param0 */
 export type Param0<T extends ((...args: any[]) => unknown) | null | undefined | false | ""> = [
     Parameters<T>,
@@ -9,4 +11,4 @@ export type Param0<T extends ((...args: any[]) => unknown) | null | undefined | 
     ? never
     : Parameters<T> extends []
     ? void
-    : Parameters<T>[0];
+    : NonUndefined<Parameters<T>[0]>;
