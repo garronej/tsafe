185f6c1f5c52771a18da5aa439a21f53ee9ca372
actions <actions@example.com>
2022-04-14 10:47:50 +0000

    Enabling shorter import paths

---
+++ capitalize.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.capitalize = void 0;
+/** @see <https://docs.tsafe.dev/capitalize> */
+function capitalize(str) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return (str.charAt(0).toUpperCase() + str.slice(1));
+}
+exports.capitalize = capitalize;
+//# sourceMappingURL=capitalize.js.map
---
+++ deno_dist/test/objectKeys.ts
@@ -0,0 +1,41 @@
+import { objectKeys } from "../objectKeys.ts";
+import { assert } from "../assert.ts";
+import { same } from "https://raw.githubusercontent.com/garronej/evt/v1.10.1/deno_dist/tools/inDepth/same.ts";
+
+{
+    type A = {
+        a: number;
+        b: number;
+        c: number;
+    };
+
+    const obj: A = {
+        "a": 1,
+        "b": 2,
+        "c": 3,
+    };
+
+    const got = objectKeys(obj);
+
+    const expected = ["a", "b", "c"];
+
+    assert(same(got, expected));
+
+    console.log("PASS TEST 1");
+}
+
+{
+    const obj = {
+        "a": 1,
+        "b": 2,
+        "c": 3,
+    };
+
+    const got = objectKeys<{ a: number; b: number }>(obj);
+
+    const expected = ["a", "b", "c"];
+
+    assert(same(got, expected));
+
+    console.log("PASS TEST 2");
+}
