bbd8d59ff11cf0566a8a9bcc2a4bb3ba217b6a9a
thieryw <thierywilliam99@gmail.com>
2022-09-08 18:50:09 +0200

    PickRequired implementation and tests

---
+++ src/test/PickRequired.ts
@@ -0,0 +1,53 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { PickRequired } from "../PickRequired";
+import { assert } from "../assert";
+import type { Equals } from "../Equals";
+
+{
+    type A = {
+        a: number;
+        b?: number;
+        method1(param: string): number;
+        method2?(params: { a: string; b: number }): typeof params;
+    };
+
+    type Expected = {
+        a: number;
+        method1(param: string): number;
+    };
+
+    type Got = PickRequired<A>;
+
+    assert<Equals<Got, Expected>>();
+}
+
+{
+    type A = {
+        a: number | undefined;
+        b?: string;
+    };
+
+    type Expected = {
+        a: number | undefined;
+    };
+
+    type Got = PickRequired<A>;
+
+    assert<Equals<Got, Expected>>();
+}
+
+{
+    interface A {
+        a: number;
+        b: string;
+        c?: unknown;
+        method1?: () => void;
+    }
+
+    //@ts-expect-error
+    type Got = PickRequired<A>;
+}
+
+//@ts-expect-error
+type X = PickRequired<number>;
