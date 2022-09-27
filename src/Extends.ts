711c3c208ef6b82426ae811b552801f1cd733531
Daniel Dietrich <cafebab3@gmail.com>
2022-09-27 23:27:57 +0200

    proper handling of any/never and more tests

--- src/Extends.ts
+++ src/Extends.ts
@@ -1,4 +1,13 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { StrictEquals } from "./tools/StrictEquals";
 
 /** https://docs.tsafe.dev/main/extends */
-export type Extends<A1, A2> = StrictEquals<A1, never> extends true ? true : A1 extends A2 ? true : false;
+export type Extends<A1, A2> = StrictEquals<A1, never> extends true
+    ? true
+    : StrictEquals<A2, never> extends true
+    ? false
+    : StrictEquals<A1, any> extends true
+    ? StrictEquals<A2, any>
+    : A1 extends A2
+    ? true
+    : false;
--- src/test/Extends.ts
+++ src/test/Extends.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import type { Equals } from "../Equals";
 import type { Extends } from "../Extends";
-import type { Not } from "../Not";
 import { assert } from "../assert";
 
 const emptyObj = {};
@@ -22,59 +21,105 @@ class A {
 
 {
     // identity
-    assert<Extends<never, never>>();
-    assert<Extends<undefined, undefined>>();
-    assert<Extends<null, null>>();
-    assert<Extends<boolean, boolean>>();
-    assert<Extends<number, number>>();
-    assert<Extends<string, string>>();
-    assert<Extends<symbol, symbol>>();
-    assert<Extends<any[], any[]>>();
-    assert<Extends<Fn, Fn>>();
-    assert<Extends<Obj, Obj>>();
-    assert<Extends<A, A>>();
+    assert<Equals<Extends<any, any>, true>>();
+    assert<Equals<Extends<never, never>, true>>();
+    assert<Equals<Extends<unknown, unknown>, true>>();
+    assert<Equals<Extends<void, void>, true>>();
+    assert<Equals<Extends<undefined, undefined>, true>>();
+    assert<Equals<Extends<null, null>, true>>();
+    assert<Equals<Extends<boolean, boolean>, true>>();
+    assert<Equals<Extends<number, number>, true>>();
+    assert<Equals<Extends<string, string>, true>>();
+    assert<Equals<Extends<symbol, symbol>, true>>();
+    assert<Equals<Extends<any[], any[]>, true>>();
+    assert<Equals<Extends<Fn, Fn>, true>>();
+    assert<Equals<Extends<Obj, Obj>, true>>();
+    assert<Equals<Extends<A, A>, true>>();
 }
 
 {
-    // never
-    assert<Equals<Extends<never, any>, true>>();
-    assert<Equals<Extends<any, never>, boolean>>();
-    assert<Equals<Extends<never, 1>, true>>();
-    assert<Equals<Extends<1, never>, false>>();
+    // never extends everything
     assert<Equals<Extends<never, never>, true>>();
+    assert<Equals<Extends<never, any>, true>>();
+    assert<Equals<Extends<never, unknown>, true>>();
+
+    // nothing but itself extends never
+    assert<Equals<Extends<never, never>, true>>();
+    assert<Equals<Extends<any, never>, false>>();
+    assert<Equals<Extends<unknown, never>, false>>();
+
+    // any extends nothing but itself
+    assert<Equals<Extends<any, any>, true>>();
+    assert<Equals<Extends<any, never>, false>>();
+    assert<Equals<Extends<any, unknown>, false>>();
+
+    // everything extends any
+    assert<Equals<Extends<any, any>, true>>();
+    assert<Equals<Extends<never, any>, true>>();
+    assert<Equals<Extends<unknown, any>, true>>();
 }
 
 {
     // numbers
-    assert<Extends<1, number>>();
-    assert<Not<Extends<number, 1>>>();
+    assert<Equals<Extends<1, number>, true>>();
+    assert<Equals<Extends<number, 1>, false>>();
 }
 
 {
     // functions
-    assert<Extends<() => any, Fn>>();
-    assert<Extends<() => void, Fn>>();
-    assert<Extends<(...args: any[]) => any, Fn>>();
-    assert<Extends<typeof fn, Fn>>();
+    assert<Equals<Extends<() => any, Fn>, true>>();
+    assert<Equals<Extends<() => void, Fn>, true>>();
+    assert<Equals<Extends<(...args: any[]) => any, Fn>, true>>();
+    assert<Equals<Extends<typeof fn, Fn>, true>>();
 
-    assert<Not<Extends<undefined, Fn>>>();
-    assert<Not<Extends<null, Fn>>>();
-    assert<Not<Extends<boolean, Fn>>>();
-    assert<Not<Extends<number, Fn>>>();
-    assert<Not<Extends<string, Fn>>>();
-    assert<Not<Extends<any[], Fn>>>();
-    assert<Not<Extends<Record<string, unknown>, Fn>>>();
-    assert<Not<Extends<A, Fn>>>();
+    assert<Equals<Extends<unknown, Fn>, false>>();
+    assert<Equals<Extends<void, Fn>, false>>();
+    assert<Equals<Extends<undefined, Fn>, false>>();
+    assert<Equals<Extends<null, Fn>, false>>();
+    assert<Equals<Extends<boolean, Fn>, false>>();
+    assert<Equals<Extends<number, Fn>, false>>();
+    assert<Equals<Extends<string, Fn>, false>>();
+    assert<Equals<Extends<any[], Fn>, false>>();
+    assert<Equals<Extends<Record<string, unknown>, Fn>, false>>();
+    assert<Equals<Extends<A, Fn>, false>>();
+}
+
+{
+    // arrays
+    assert<Equals<Extends<any[], any[]>, true>>();
+    assert<Equals<Extends<[], []>, true>>();
+    assert<Equals<Extends<any[], []>, false>>();
+    assert<Equals<Extends<[], any[]>, true>>();
+    assert<Equals<Extends<[], [any]>, false>>();
+    assert<Equals<Extends<[any], any[]>, true>>();
+    assert<Equals<Extends<[any], []>, false>>();
+
+    assert<Equals<Extends<[1], [number]>, true>>();
+    assert<Equals<Extends<[number], [1]>, false>>();
+    assert<Equals<Extends<[1], [1]>, true>>();
+    assert<Equals<Extends<[1], [2]>, false>>();
+
+    assert<Equals<Extends<[1, ""], [number, string]>, true>>();
+    assert<Equals<Extends<[1, ""], [number, any]>, true>>();
+    assert<Equals<Extends<[1, ""], [number, never]>, false>>();
+    assert<Equals<Extends<[number, string], [1, ""]>, false>>();
+    assert<Equals<Extends<[number, any], [1, ""]>, false>>();
+    assert<Equals<Extends<[number, never], [1, ""]>, false>>();
 }
 
 {
     // objects
-    assert<Extends<A, EmptyObj>>();
-    assert<Not<Extends<EmptyObj, A>>>();
+    assert<Equals<Extends<A, EmptyObj>, true>>();
+    assert<Equals<Extends<EmptyObj, A>, false>>();
 
-    assert<Extends<A, Record<string, any>>>();
-    assert<Not<Extends<Record<string, any>, A>>>();
+    assert<Equals<Extends<A, Record<string, any>>, true>>();
+    assert<Equals<Extends<Record<string, any>, A>, false>>();
 
-    assert<Extends<A, unknown>>();
-    assert<Not<Extends<unknown, A>>>();
+    assert<Equals<Extends<A, unknown>, true>>();
+    assert<Equals<Extends<unknown, A>, false>>();
+
+    assert<Equals<Extends<EmptyObj, { a: 1 }>, false>>();
+    assert<Equals<Extends<{ a: 1 }, EmptyObj>, true>>();
+    assert<Equals<Extends<{ a: 1 }, { a: number }>, true>>();
+    assert<Equals<Extends<{ a: 1 }, { a: string }>, false>>();
 }
