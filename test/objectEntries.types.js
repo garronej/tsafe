cc87e123a8496b80d100071bdd694859ce4cf9bc
actions <actions@example.com>
2022-09-29 12:28:58 +0000

    Enabling shorter import paths

---
+++ test/objectEntries.types.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var objectEntries_1 = require("../objectEntries");
+var assert_1 = require("../assert");
+{
+    var o = {
+        "a": "foo",
+        "b": 33,
+        "c": true,
+    };
+    var entries = (0, objectEntries_1.objectEntries)(o);
+    (0, assert_1.assert)();
+}
+{
+    var o = {
+        "a": "foo",
+        "b": 33,
+        "c": true,
+    };
+    var entries = (0, objectEntries_1.objectEntries)(o);
+    (0, assert_1.assert)();
+}
+{
+    (0, assert_1.assert)();
+}
+//# sourceMappingURL=objectEntries.types.js.map
