3b0af39f67bc14b6054d45714bcc47adda6d5ab2
garronej <joseph.garrone.gj@gmail.com>
2022-10-10 00:06:11 +0200

    Fix objectEntries #17

--- src/objectEntries.ts
+++ src/objectEntries.ts
@@ -3,9 +3,12 @@
 /** https://docs.tsafe.dev/objectentries */
 export function objectEntries<O extends Record<string, any>>(
     o: O,
-): { [Key in keyof O]: [Key, O[Key]] }[keyof O] {
+): Exclude<{ [Key in keyof O]: [Key, O[Key]] }[keyof O], undefined>[] {
     return Object.entries(o) as any;
 }
 
 /** Return type of objectEntries https://docs.tsafe.dev/objectentries */
-export type ObjectEntries<O extends Record<string, any>> = { [Key in keyof O]: [Key, O[Key]] }[keyof O];
+export type ObjectEntries<O extends Record<string, any>> = Exclude<
+    { [Key in keyof O]: [Key, O[Key]] }[keyof O],
+    undefined
+>[];
--- src/test/objectEntries.types.ts
+++ src/test/objectEntries.types.ts
@@ -1,35 +1,78 @@
 import type { Equals, ObjectEntries } from "..";
 import { objectEntries } from "../objectEntries";
 import { assert } from "../assert";
+import { Reflect } from "../Reflect";
 
 {
-    const o = {
-        "a": "foo",
-        "b": 33,
-        "c": true,
-    };
+    const input = Reflect<{
+        a: string;
+        b: number;
+        c: boolean;
+    }>();
 
-    const entries = objectEntries(o);
+    const got = objectEntries(input);
 
-    assert<Equals<typeof entries, ["a", string] | ["b", number] | ["c", boolean]>>();
+    type Expected = (["a", string] | ["b", number] | ["c", boolean])[];
+
+    assert<Equals<typeof got, Expected>>();
 }
 
 {
-    const o = {
+    const input = Reflect<{
+        a?: string;
+        b: number;
+        c: boolean;
+    }>();
+
+    const got = objectEntries(input);
+
+    type Expected = (["a", string | undefined] | ["b", number] | ["c", boolean])[];
+
+    assert<Equals<typeof got, Expected>>();
+}
+
+{
+    const input = {
         "a": "foo",
         "b": 33,
         "c": true,
     } as const;
 
-    const entries = objectEntries(o);
+    assert<Equals<typeof input, { readonly a: "foo"; readonly b: 33; readonly c: true }>>();
 
-    assert<Equals<typeof entries, ["a", "foo"] | ["b", 33] | ["c", true]>>();
+    const got = objectEntries(input);
+
+    type Expected = (["a", "foo"] | ["b", 33] | ["c", true])[];
+
+    assert<Equals<typeof got, Expected>>();
+}
+
+{
+    const input = {};
+
+    const got = objectEntries(input);
+
+    type Expected = never[];
+
+    assert<Equals<typeof got, Expected>>();
 }
 
 {
     type Input = { a: string; b: number; c: boolean };
 
-    type Expect = ["a", string] | ["b", number] | ["c", boolean];
+    type Got = ObjectEntries<Input>;
 
-    assert<Equals<ObjectEntries<Input>, Expect>>();
+    type Expected = (["a", string] | ["b", number] | ["c", boolean])[];
+
+    assert<Equals<Got, Expected>>();
+}
+
+{
+    type Input = { a?: string; b: number; c: boolean };
+
+    type Got = ObjectEntries<Input>;
+
+    type Expected = (["a", string | undefined] | ["b", number] | ["c", boolean])[];
+
+    assert<Equals<Got, Expected>>();
 }
