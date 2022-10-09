b7364fb68d48e4ba87cec4b7c604e5b7cf4ad22b
Damian Jarosz <damian.jarosz@alpaka.in>
2022-10-09 12:02:50 +0200

    Fixed IsPick. Working more examples.

--- src/IsPick.ts
+++ src/IsPick.ts
@@ -5,6 +5,6 @@ import { StrictEquals } from "./tools/StrictEquals";
  * IsPick<{ p1: string; p2?: string; }, { p1: string; p2?: string; p3?: number; p4: string; }>
  * Checks if type X is Pick<Y, K> of type Y, where K is any set of properties of type Y
  */
-export type IsPick<A1, A2> = StrictEquals<A1, Pick<A2, keyof A2>> extends true
+export type IsPick<A1, A2 extends A1> = StrictEquals<A1, Pick<A2, keyof A1>> extends true
     ? true
     : false;
