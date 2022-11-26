91576a6fbb08919194b7bd78f46c749ed2b445de
garronej <joseph.garrone@data.gouv.fr>
2022-11-26 14:29:47 +0100

    Attempt to release for both ESM and CJS

--- src/res/Equals_example3.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/ban-types */
-import type { Equals } from "..";
-import { assert } from "../assert";
-
-declare function theTestedFunction<Key extends string>(): Record<Key, string>;
-
-const out = theTestedFunction<"foo" | "bar">();
-
-assert<Equals<typeof out, { foo: string; bar: string }>>();
