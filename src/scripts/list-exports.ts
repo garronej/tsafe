33b586e3d7154397ec52281c923e7d4b50afc427
garronej <joseph.garrone@data.gouv.fr>
2022-11-29 11:39:15 +0100

    Update list exports

--- src/scripts/list-exports.ts
+++ src/scripts/list-exports.ts
@@ -10,6 +10,7 @@ const packageJsonFilePath = pathJoin(getProjectRoot(), "package.json");
 const packageJsonParsed = JSON.parse(fs.readFileSync(packageJsonFilePath).toString("utf8"));
 
 const srcDirPath = pathJoin(getProjectRoot(), "src");
+const labDirPath = pathJoin(srcDirPath, "lab");
 
 const newExports = {
     ".": {
@@ -46,6 +47,38 @@ const newExports = {
             .map(name => [
                 `./${name}`,
                 { "require": `./dist/${name}.js`, "import": `./dist/esm/${name}.js` },
+            ]),
+    ),
+    ...Object.fromEntries(
+        fs
+            .readdirSync(labDirPath)
+            .filter(basename => {
+                try {
+                    execSync(`git ls-files --error-unmatch ${pathJoin(labDirPath, basename)}`, {
+                        "stdio": [],
+                    });
+                } catch {
+                    return false;
+                }
+
+                return true;
+            })
+            .map(basename => {
+                const match = basename.match(/^([^.]+)\.tsx?$/);
+
+                if (match === null) {
+                    return undefined;
+                }
+
+                return match[1];
+            })
+            .filter(exclude(undefined))
+            .filter(exclude("index"))
+            .sort()
+            .reverse()
+            .map(name => [
+                `./lab/${name}`,
+                { "require": `./dist/lab/${name}.js`, "import": `./dist/esm/lab/${name}.js` },
             ]),
     ),
 };
