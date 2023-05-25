a93cedb1816aeae279223ed9a059c19634f1e29d
actions <actions@github.com>
2023-05-25 07:15:25 +0000

    Adding deno distribution files and moving files from /dist to /

---
+++ deno_dist/lab/index.ts
@@ -0,0 +1,5 @@
+export * from "./defineAccessors.ts";
+export * from "./overwriteReadonlyProp.ts";
+export * from "./PromiseOrNot.ts";
+export * from "./UnpackTypeGuard.ts";
+export * from "./withDefaults.ts";
