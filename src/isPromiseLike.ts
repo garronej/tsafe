fa8d40281e5e87c04f8b05bf4cb6160dadc603b3
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-01-15 18:51:55 +0100

    Softer test for isPromiseLike

--- src/isPromiseLike.ts
+++ src/isPromiseLike.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function isPromiseLike<T = void>(o: any): o is PromiseLike<T> {
-    return typeof o?.then === "function";
+    return typeof o === "object" && o !== null && "then" in o && typeof o.then === "function";
 }
