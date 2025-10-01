c8a00dfc2b6e6ad9506eebf0619c332efaeb7504
actions <actions@github.com>
2025-10-01 13:42:15 +0000

    Adding deno distribution files and moving files from /dist to /

---
+++ deno_dist/UnionToIntersection.ts
@@ -0,0 +1,6 @@
+/** @see <https://docs.tsafe.dev/uniontointersection> **/
+export type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
+    k: infer I,
+) => void
+    ? I
+    : never;
